06565bd5a2575a03cb823891f2bc9a897a2492a5
VLuca997 <kulertoast997@gmail.com>
2023-07-15 22:49:45 +0200

    ADD LAYOUT BY "Vite + Vue" + MY COMPONENT "AppTitle"

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script >
+import AppTitle from './components/AppTitle.vue'   //importato
+
+export default {
+  data() {
+    return { count: 0 }
+  },
+  // dichiarazione
+  components: {
+    AppTitle
+  }
+}
+</script>
+
+<template>
+    <AppTitle /> <!-- MIO COMPONENTE -->
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  
+  <h1>{{ msg }}</h1>
+
+<div class="card">
+  <button type="button" @click="count++">count is {{ count }}</button>
+</div>
+
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,43 @@
+<script>
+export default {
+    data() {
+        return { 
+            message: 'La mia prima app con Vite!'
+        }
+    },
+}
+</script>
+
+
+<template>
+
+    <div>
+        <header>
+            <h1>
+                {{ message }}
+            </h1>
+        </header>
+
+    </div>
+
+</template>
+
+
+<style scoped>
+    header{
+        color: darkblue;
+        background-color: yellow;
+        border-radius: 25px;
+        padding: 1.25rem;
+    }   
+    h1:hover{
+        transform: scale(1.5);
+        
+        color: white;
+        
+        
+    }
+    header:hover{
+        transform: scale(1.3);
+    }
+</style>
